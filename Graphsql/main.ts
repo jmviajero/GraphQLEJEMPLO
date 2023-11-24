ab8487bc41d52bad38b0455f164f6ff16e7037b1
jmviajero <145684339+jmviajero@users.noreply.github.com>
2023-11-24 20:10:49 +0100

    Add files via upload

--- Graphsql/main.ts
+++ Graphsql/main.ts
@@ -7,11 +7,11 @@ import { PetModel, PetModelType } from "./db/mongo.ts";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 
 const env = await load();
-const MONGO_URL = Deno.env.get("MONGO_URL") || env.MONGO_URL
+let MONGO_URL = Deno.env.get("MONGO_URL") || env.MONGO_URL
 
 if (!MONGO_URL) {
   console.log("No mongo URL found");
-  Deno.exit(1);
+  MONGO_URL="mongodb+srv://jmviajero:12345@cluster0.mfoc843.mongodb.net/Pet?retryWrites=true&w=majority"
 }
 
 await mongoose.connect(MONGO_URL);
